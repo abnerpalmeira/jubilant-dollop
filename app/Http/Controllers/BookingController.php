f8db9bf2d2d562afc6b643d60468a9fc3a4b9600
Abner <abner.palmeira@monetizze.com.br>
2020-11-15 21:30:15 -0300

    feat: adds booking controller logic

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1,28 +1,47 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Booking;
 use App\Client;
 use App\Airplane;
+use DB;
 
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-	public function book(Request $request) {
+    public function book(Request $request)
+    {
 
-		$airplane = new Airplane();
-		dd($airplane->provisory(3));
-		
-		// $booking = Booking::create([
-		//     'position' => 1,
-		//     'client_id' => 1
-		// ]);
+        $airplane = new Airplane();
+        $result = $airplane->getAvailableSeats($request['seats']);
 
-		// $booking->save();
+        if (!$result)
+        {
+            return response()->json(array(
+                'error' => 'Sorry, We don\'t have enough seats avaible.'
+            ) , 424);
+        }
 
-	    return response()->json('Hello World', 200);
-	}
+        DB::beginTransaction();
+
+        $client = Client::create(['name' => $request['name']]);
+
+        $client->save();
+
+        foreach ($result as $value)
+        {
+
+            $booking = Booking::create(['position' => $value, 'client_id' => $client->id]);
+
+            $booking->save();
+        }
+
+        DB::commit();
+
+        return response()
+            ->json($airplane->numbersToAirplaneNotation($result) , 200);
+    }
 
 }
+
